3c8eadedaa60582886106c165fe0372dca69236a
rro <r.rogers@iks-gmbh.com>
2019-09-23 15:04:13 +0200

    isThisCellAlive() now returns a boolean

--- GameOfLifeKata/src/com/rroIKS/GameOfLife.java
+++ GameOfLifeKata/src/com/rroIKS/GameOfLife.java
@@ -5,10 +5,10 @@ public class GameOfLife {
     private boolean[][] calculationMatrix;
 
     GameOfLife(boolean[][] inputGameBoard) {
-        this.calculationMatrix = new boolean [inputGameBoard.length+2][inputGameBoard[0].length+2];
+        this.calculationMatrix = new boolean[inputGameBoard.length + 2][inputGameBoard[0].length + 2];
         for (int i = 0; i < inputGameBoard.length; i++) {
             for (int j = 0; j < inputGameBoard[i].length; j++) {
-                calculationMatrix[i+1][j+1] = inputGameBoard[i][j];
+                calculationMatrix[i + 1][j + 1] = inputGameBoard[i][j];
             }
         }
     }
@@ -17,24 +17,22 @@ public class GameOfLife {
         boolean[][] tempForNextGeneration = new boolean[calculationMatrix.length][calculationMatrix[0].length];
         for (int i = 1; i < calculationMatrix.length - 1; i++) {
             for (int j = 1; j < calculationMatrix[i].length - 1; j++) {
-                isThisCellAlive(tempForNextGeneration, i, j);
+                tempForNextGeneration[i][j] = isThisCellAliveNextGeneration(i, j);
             }
         }
         calculationMatrix = tempForNextGeneration;
     }
 
-    private void isThisCellAlive(boolean[][] tempForNextGeneration, int i, int j) {
-        int numberOfNeighbors = calculateNumberOfNeighbors(i,j);
+    private boolean isThisCellAliveNextGeneration(int i, int j) {
+        int numberOfNeighbors = calculateNumberOfNeighbors(i, j);
         if (calculationMatrix[i][j]) {
             if (numberOfNeighbors <= 1) {
-                tempForNextGeneration[i][j] = false;
+                return false;
             } else {
-                tempForNextGeneration[i][j] = numberOfNeighbors < 4;
+                return numberOfNeighbors < 4;
             }
         } else {
-            if (numberOfNeighbors == 3) {
-                tempForNextGeneration[i][j] = true;
-            }
+            return numberOfNeighbors == 3;
         }
     }
 
@@ -56,10 +54,10 @@ public class GameOfLife {
 
 
     public boolean[][] getGameBoard() {
-        boolean[][] gameBoard = new boolean[calculationMatrix.length-2][calculationMatrix[0].length -2];
+        boolean[][] gameBoard = new boolean[calculationMatrix.length - 2][calculationMatrix[0].length - 2];
         for (int i = 1; i < calculationMatrix.length - 1; i++) {
             for (int j = 1; j < calculationMatrix[i].length - 1; j++) {
-               gameBoard[i-1][j-1] = calculationMatrix[i][j];
+                gameBoard[i - 1][j - 1] = calculationMatrix[i][j];
             }
         }
         return gameBoard;
